0f6b0e8bb2a5c266a25d5275a84a5d002becf028
Rodrigo Camara <rodrigo.camarar@fluxit.com.ar>
2021-05-09 18:02:16 -0300

    Added scroll view with: title view, image view and price view.

--- MeLi/ProductDetail/View/ProductDetailView.swift
+++ MeLi/ProductDetail/View/ProductDetailView.swift
@@ -8,17 +8,30 @@
 import UIKit
 
 class ProductDetailView: UIViewController {
-
+    
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    
     var product: Product?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavBar()
+        configureUI()
     }
     
     private func configureNavBar() {
         title = "Detalle del Producto"
         self.navigationController?.navigationBar.tintColor = .black
+    }
+    
+    private func configureUI() {
+        productTitle.text = product?.title
     }
 
 }
